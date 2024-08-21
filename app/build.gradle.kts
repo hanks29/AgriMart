e21739d319f39599524f302e5780c654689451c3
hhlong2903 <hhl.270203@gmai.com>
2024-08-22 00:09:38 +0700

    Thêm chức năng đăng nhập và đăng ký

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.viewpager:viewpager:1.0.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/agrimart/ui/MainActivity.java
+++ app/src/main/java/com/example/agrimart/ui/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.agrimart.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.agrimart.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Tạo một Handler để trì hoãn việc chuyển đổi activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Chuyển đến activity mới
+                Intent intent = new Intent(MainActivity.this, Login_Register.class);
+                startActivity(intent);
+                finish(); // Kết thúc MainActivity để người dùng không quay lại nó
+            }
+        }, 2000); // Thời gian trì hoãn (2000ms = 2 giây)
     }
 }

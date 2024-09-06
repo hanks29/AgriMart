bf7f11a5bbb9a1e43694a3dcb308e46a70df4862
Khánh <leqkhanh0109@gmail.com>
2024-09-06 12:29:42 +0700

    update

---
+++ app/src/main/java/com/example/agrimart/View/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.agrimart.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.agrimart.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/agrimart/View/SplashScreen/SplashScreenActivity.java
+++ app/src/main/java/com/example/agrimart/View/SplashScreen/SplashScreenActivity.java
@@ -1,4 +1,4 @@
-package com.example.agrimart.ui;
+package com.example.agrimart.View.SplashScreen;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.agrimart.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_splash_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

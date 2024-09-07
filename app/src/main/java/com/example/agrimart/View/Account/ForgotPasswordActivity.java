3a62bc41dac9eca23b19edaf3193dd4c943349bd
Khánh <leqkhanh0109@gmail.com>
2024-09-07 13:54:35 +0700

    update Main, BottomNavigation, Signin, signup, forgotpass, otp UI

--- app/src/main/java/com/example/agrimart/View/Account/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/agrimart/View/Account/ForgotPasswordActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,21 +13,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.agrimart.R;
-import com.example.agrimart.View.MainActivity;
 
-public class SignInActivity extends AppCompatActivity {
-    Button btnDonthaveAccount, btnSignIn;
-    TextView forgotPass;
+public class ForgotPasswordActivity extends AppCompatActivity {
+    Button btnContinue;
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_forgot_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 
     @Override
@@ -38,34 +38,26 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void addControls() {
-        btnDonthaveAccount = findViewById(R.id.dontHaveAccount);
-        forgotPass = findViewById(R.id.forgotPassword);
-        btnSignIn = findViewById(R.id.btnSignIn);
+        btnContinue = findViewById(R.id.btn_continue);
+        btnBack = findViewById(R.id.btn_back);
     }
 
     public void addEvents() {
-        btnDonthaveAccount.setOnClickListener(new View.OnClickListener() {
+        btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(ForgotPasswordActivity.this, EnterOTPActivity.class);
                 startActivity(intent);
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
 
-        forgotPass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, ForgotPasswordActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
     }
+
+
 }

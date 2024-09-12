624f9885126978e09cd80288b66f4ef8d4293825
Khánh <leqkhanh0109@gmail.com>
2024-09-12 15:31:09 +0700

    update

--- app/src/main/java/com/example/agrimart/ui/MainActivity.java
+++ app/src/main/java/com/example/agrimart/ui/MainActivity.java
@@ -1,26 +1,82 @@
 package com.example.agrimart.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.agrimart.R;
+import com.example.agrimart.ui.Homepage.HomeFragment;
+import com.example.agrimart.ui.PostProduct.PostProductActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+            bottomNavigationView.setSelectedItemId(R.id.home);
+        }
+
+        addControls();
+        addEvents();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    private boolean onNavigationItemSelected(MenuItem item) {
+        Fragment selectedFragment = null;
+
+        if (item.getItemId() == R.id.home) {
+            selectedFragment = new HomeFragment();
+        }
+        else if (item.getItemId() == R.id.post) {
+            Intent intent = new Intent(MainActivity.this, PostProductActivity.class);
+            startActivity(intent);
+         }
+
+        if (selectedFragment != null) {
+            loadFragment(selectedFragment);
+        }
+
+        return true;
+    }
+
+    void addControls()
+    {
+    }
+
+    void addEvents()
+    {
+
     }
 }

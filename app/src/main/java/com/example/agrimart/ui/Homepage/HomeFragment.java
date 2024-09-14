3b120d3880e9f36b096776c9df8986de3ad8f19c
Khánh <leqkhanh0109@gmail.com>
2024-09-14 18:34:51 +0700

    update product detail page, cart fragment, notification activity

--- app/src/main/java/com/example/agrimart/ui/Homepage/HomeFragment.java
+++ app/src/main/java/com/example/agrimart/ui/Homepage/HomeFragment.java
@@ -1,11 +1,13 @@
 package com.example.agrimart.ui.Homepage;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,7 @@ import com.example.agrimart.data.adapter.ProductAdapter;
 import com.example.agrimart.data.model.Category;
 import com.example.agrimart.data.model.Product;
 import com.example.agrimart.databinding.FragmentHomeBinding;
+import com.example.agrimart.ui.ProductPage.ProductDetailActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,11 +67,19 @@ public class HomeFragment extends Fragment {
         products.add(new Product(R.drawable.banana,"Organic Bananas","45.000","200kg","20 người đang hỏi"));
         products.add(new Product(R.drawable.banana,"Organic Bananas","45.000","200kg","20 người đang hỏi"));
         products.add(new Product(R.drawable.banana,"Organic Bananas","45.000","200kg","20 người đang hỏi"));
-        ProductAdapter productAdapter=new ProductAdapter(products);
+        products.add(new Product(R.drawable.banana,"Organic Bananas","45.000","200kg","20 người đang hỏi"));
+        products.add(new Product(R.drawable.banana,"Organic Bananas","45.000","200kg","20 người đang hỏi"));
+
+        ProductAdapter productAdapter=new ProductAdapter(products, product -> {
+            Intent intent=new Intent(getContext(), ProductDetailActivity.class);
+            startActivity(intent);
+        });
+
         binding.rvProducts.setLayoutManager(new GridLayoutManager(getContext(),2));
 
         binding.rvProducts.addItemDecoration(new SpacesItemDecoration(spacingInPixels));
         binding.rvProducts.setAdapter(productAdapter);
         return view;
     }
+
 }
---
+++ app/src/main/java/com/example/agrimart/viewmodel/NotificationViewModel.java
@@ -0,0 +1,7 @@
+package com.example.agrimart.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class NotificationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

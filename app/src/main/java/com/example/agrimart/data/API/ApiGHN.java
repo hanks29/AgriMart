912482d6e079a4733a35db1f908b793edbffff7e
duykhanh <duykhanhz112233@gmail.com>
2024-11-17 22:30:55 +0700

    update key

--- app/src/main/java/com/example/agrimart/data/API/ApiGHN.java
+++ app/src/main/java/com/example/agrimart/data/API/ApiGHN.java
@@ -11,7 +11,7 @@ public class ApiGHN {
     public static Retrofit getClient(){
         if(retrofit==null){
             retrofit=new Retrofit.Builder()
-                    .baseUrl("https://online-gateway.ghn.vn/shiip/public-api/")
+                    .baseUrl("https://dev-online-gateway.ghn.vn/shiip/public-api/")
                     .client(new OkHttpClient())
                     .addConverterFactory(JacksonConverterFactory.create())
                     .build();
--- app/src/main/java/com/example/agrimart/data/API/ConfigGHN.java
+++ app/src/main/java/com/example/agrimart/data/API/ConfigGHN.java
@@ -1,6 +1,6 @@
 package com.example.agrimart.data.API;
 
 public class ConfigGHN {
-    public static final String TOKEN_SHOP_GHN="199acb3e-3b64-11ef-a3f8-86d541dd462b";
-    public static final String SHOP_ID="5151825";
+    public static final String TOKEN_SHOP_GHN="1b58e6e8-a4f8-11ef-8e53-0a00184fe694";
+    public static final String SHOP_ID="195361";
 }
